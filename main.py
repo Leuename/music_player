c0233de2698e1d17daefcf2c3e61d11570580e53
Emmanuel Millave <emmanuel.millave@ams-osram.com>
2026-03-30 16:18:27 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from pathlib import Path
+
+class RepeatMode(Enum):
+        
+    no_repeat = 0
+    repeat_one = 1
+    repeat_all = 2
+
+class Song():
+    def __init__(self, title, artistName, duration, filePath, genre):
+        self.title = title()
+        self.artistName = artistName()
+        self.duration = duration()
+        self.filePath = filePath()
+        self.genre = genre()
+
+    def _getTitle(self):
+        pass
+
+    def _getArtistName(self):
+        pass
+
+    def _getFilePath(self , filepath):
+        file_part = ""
+    
+    def _getGenre(self):
+        pass
+
+
+
+    def getDisplayName(self):
+        pass
+
+    def _getDuration(self):
+        pass
---
+++ try_path.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+print("The file path is:")
+
+print(Path.cwd())
